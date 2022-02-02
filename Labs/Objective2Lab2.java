cbfeeaaa0733fb576db79c87906695bee9112dc7
Acorneld <acorneld@gmail.com>
2022-02-02 15:17:34 -0700

    Objective2Lab1&2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Andrew Cornelius";
+    int age = 33;
+    String state = "Colorado";
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("State: " + state);
+
+
+  }
+}
