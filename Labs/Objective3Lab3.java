a06c03808210f343c4d5f2df113b055efcac7e6d
Acorneld <acorneld@gmail.com>
2022-02-03 14:37:58 -0700

    Objective3Lab3Finishedcleanercode

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -5,13 +5,6 @@ public class Objective3Lab3 {
      second = 1 + (int)(Math.random() * 6);
      third = 1 + (int)(Math.random() * 6);
       sum = first + second + third;
-       System.out.print(first);
-       System.out.print(" + ");
-       System.out.print(second);
-       System.out.print(" + ");
-       System.out.print(third);
-       System.out.print(" = ");
-       System.out.println(sum);
-      
+       System.out.println(first + " + " + second + " + " + third + " + " + " = " + sum);
   }
 }
