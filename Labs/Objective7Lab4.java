a41cdaa9bb96dc444dcfda4e924872af68fb0f32
Acorneld <acorneld@gmail.com>
2022-02-06 16:31:37 -0700

    Objective7Lab4Finished

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,21 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int count, sum;
+    sum = 0;
+    count = 1;
+
+    do { sum = sum + count;
+
+      count += 1;
+      }
+    while (count <= 20);
+
+    System.out.println(sum);
+
+
+
+
+  }
+
+}
