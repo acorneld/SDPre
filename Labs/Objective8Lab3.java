374d5fde6a06da638f7eae62e9aac01d7738534f
Acorneld <acorneld@gmail.com>
2022-02-07 13:13:04 -0700

    Objective8Lab3Finished

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,12 @@
+public class Objective8Lab3{
+  public static void main(String[] args){
+
+    for(int i = 1; i <= 20; i+=1) {
+
+      if(i % 2 == 1) {System.out.println(i + " the number is odd ");
+}
+      else if(i % 2 == 0) {System.out.println(i + " the number is even");
+}
+    }
+  }
+}
